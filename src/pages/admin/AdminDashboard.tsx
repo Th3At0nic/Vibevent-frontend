854d0afd8939cc46ed913997fdb52c7161a0e04b
Th3At0nic <islammdrahatul@gmail.com>
2025-06-30 23:25:49 +0600

    changed the tailwin version to v4, fixed previous issue of tailwind doesnt work properly

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,7 +1,9 @@
 const AdminDashboard = () => {
   return (
     <div>
-      <h1>This is admin dashboard</h1>
+      <h1 className="text-center text-4xl  text-red-500">
+        This is admin dashboard
+      </h1>
     </div>
   );
 };
--- src/pages/user/UserDashboard.tsx
+++ src/pages/user/UserDashboard.tsx
@@ -1,7 +1,7 @@
 const UserDashboard = () => {
   return (
     <div>
-      <h1>This is userDashboard</h1>
+      <h1 className="text-center text-4xl p-10 m-10">This is userDashboard</h1>
     </div>
   );
 };
